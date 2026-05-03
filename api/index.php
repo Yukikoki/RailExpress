612f71bd05cb6dcf905ffb960d2ddeb0bd476e42
Yukioki <yukikoseptiano28@gmail.com>
2026-05-03 22:03:20 +0700

    Fix: Hardcode tmp paths in api/index

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
-// Pastikan path ini benar-benar mengarah ke public/index.php
+
+// Masukkan ini di paling atas setelah <?php
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+
 require __DIR__ . '/../public/index.php';
