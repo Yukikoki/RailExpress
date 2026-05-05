331f014653a72163c550b82d46c53960a289a25d
Yukioki <yukikoseptiano28@gmail.com>
2026-05-05 22:29:55 +0700

    Fix: move view cache to /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,22 @@
 <?php
+
+// Pastikan folder-folder yang dibutuhkan Laravel ada di /tmp
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+
+// Redirect Laravel untuk menggunakan folder /tmp tersebut
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('CACHE_DRIVER=file');
+putenv('SESSION_DRIVER=file');
+
 require __DIR__ . '/../public/index.php';
